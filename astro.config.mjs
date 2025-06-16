4c03a51c3959e13c2df5f7e5da01757cf5ed5126
lbm <3095088766@qq.com>
2025-06-16 19:07:11 +0800

    部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://liangbm3.github.io',
   // base: '/docs',
   trailingSlash: 'never',
 
